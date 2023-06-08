9ca6703a9d640e50185207a1ad64b9cd5aacc5fc
Juan Aste <juanpabloaste00@gmail.com>
2023-06-08 15:53:52 -0300

    deploy

--- src/models/Users.js
+++ src/models/Users.js
@@ -14,17 +14,12 @@ module.exports = (sequelize) => {
       },
       userName: {
         type: DataTypes.STRING,
-        allowNull: false,
         unique: true,
       },
       email:{
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
-      },
-      password:{
-        type: DataTypes.STRING,
-        allowNull: false,
       },
       phone:{
         type: DataTypes.STRING,
