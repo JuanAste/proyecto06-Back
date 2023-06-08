ec250b0804d82e6a53d593ee74f7863f9005a385
Juan Aste <juanpabloaste00@gmail.com>
2023-06-08 18:20:44 -0300

    commit arreglar

--- src/Controllers/tablaFiltro/getFilters.js
+++ src/Controllers/tablaFiltro/getFilters.js
@@ -1,10 +1,9 @@
 const { Filtros } = require("../../db");
 
-
 const getFiltro = async (req, res) => {
   try {
-    const {name} = req.query
-    const propiedadesfiltro = await Filtros.findOne({where:{name}})
+    const { name } = req.query;
+    const propiedadesfiltro = await Filtros.findOne({ where: { name } });
     res.status(200).json(propiedadesfiltro);
   } catch (error) {
     res.status(500).json({ error: error.messaje });
