9b29b4f7ad975e4a6f2b860832fb3f109c12e6a0
Juan Aste <juanpabloaste00@gmail.com>
2023-06-08 18:09:29 -0300

    preLoad user filters

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const { productos } = require("./src/preLoad/products");
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
-const { Products, Users } = require("./src/db.js");
-
+const { Products, Users, Filtros } = require("./src/db.js");
+const { users } = require("./src/preLoad/users");
+const { filtros } = require("./src/preLoad/filtros");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: true }).then(() => {
   //force: true
   server.listen(port, () => {
     console.log(`%s listening at ${port}`);
@@ -17,31 +18,44 @@ conn.sync({ force: true }).then(async () => {
         name: producto.name,
       },
       defaults: {
-        sells:producto.sells,
+        sells: producto.sells,
         name: producto.name,
         price: producto.price,
         images: producto.images,
         stock: producto.stock,
-        type:producto.type,
+        type: producto.type,
         amount: producto.amount,
-        brand:producto.brand,
-        alcoholContent:producto.alcoholContent,
+        brand: producto.brand,
+        alcoholContent: producto.alcoholContent,
         Variety: producto.Variety,
         container: producto.container,
         description: producto.description,
         ableDiscount: producto.ableDiscount,
-        percentageDiscount:producto.percentageDiscount,
-        availability: producto.availability
+        percentageDiscount: producto.percentageDiscount,
+        availability: producto.availability,
       },
     });
   });
-  await Users.findOrCreate({
-    where: {
-      userName: "Juan",
-    },
-    defaults:{
-      userName: "Juan",
-      email: "juan@gmail.com",
-    }
-  })
+  users.forEach(async (user) => {
+    await Users.findOrCreate({
+      where: {
+        userName: user.userName,
+      },
+      defaults: {
+        userName: user.userName,
+        email: user.email,
+      },
+    });
+  });
+  filtros.forEach(async (filtro) => {
+    await Filtros.findOrCreate({
+      where: {
+        name: filtro.name,
+      },
+      defaults: {
+        name: filtro.name,
+        data: filtro.data,
+      },
+    });
+  });
 });
